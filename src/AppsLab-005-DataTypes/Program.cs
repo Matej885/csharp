759760a3dd55450961235a68826f284279807e74
Matej885 <matej.sustek999@gmail.com>
2025-09-10 13:08:44 +0200

    premenne 3

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -6,7 +6,8 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            int age = 15;
+            /*
+            int age = (15);
             Console.WriteLine(age);
 
             float pi = 3.14f;
@@ -20,7 +21,32 @@ namespace MyApp
 
             bool isAdult = false;
             Console.WriteLine(isAdult);
+            */
+            int a = 10;
+            int b = 1;
 
+            int sum = a + b;
+            Console.WriteLine(sum);
+
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine("Matej");
+
+            int birthDay = 1;
+            int birthMonth = 5;
+            int birthYear = 2010;
+
+            int birthsum = birthDay + birthMonth + birthYear;
+            Console.WriteLine(birthsum);
+            Console.WriteLine(birthsum * 10);
+            Console.WriteLine((birthDay + birthMonth) * birthYear);
+            Console.WriteLine(5f / 3f);
+            string meno = ("Matej");
+            Console.WriteLine(meno + birthYear);
+            Console.WriteLine(meno + (birthMonth + birthYear));
+            bool result = 6 != 6;
+            Console.WriteLine(result);
             Console.ReadKey();
         }
     }
